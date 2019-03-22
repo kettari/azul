80ab3c5168307f272bcf8d9da7ee7bcf8d82b5b8
Andy <ant@kaula.ru>
2019-03-22 17:25:28 +0300

    Fixed URL regex

--- src/AppBundle/Endpoint/ShortcutCreateEndpoint.php
+++ src/AppBundle/Endpoint/ShortcutCreateEndpoint.php
@@ -69,8 +69,7 @@ class ShortcutCreateEndpoint extends AbstractSecuredApiEndpoint implements PostM
        *
        * @link https://mathiasbynens.be/demo/url-regex
        */
-      if (!preg_match('_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS',
-        $item['url'])) {
+      if (!preg_match($this->getUrlRegex(), $item['url'])) {
         throw new BadRequestHttpException(sprintf('Item "%s" has invalid value in the field "url" = "%s"',
           $key, $item['url']));
       }
@@ -102,6 +101,14 @@ class ShortcutCreateEndpoint extends AbstractSecuredApiEndpoint implements PostM
     $this->inputData = $json;
 
     return $this;
+  }
+
+  /**
+   * Returns regex to validate URL
+   * @return string
+   */
+  private function getUrlRegex() {
+    return '%^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@|\d{1,3}(?:\.\d{1,3}){3}|(?:(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)(?:\.(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)*(?:\.[a-z\x{00a1}-\x{ffff}]{2,6}))(?::\d+)?(?:[^\s]*)?$%iu';
   }
 
   /**
